240a161dbb2a7e8bd77ded5924793e7a3c0fd635
Azlee Babar <azleeb@gmail.com>
2026-05-29 15:49:29 +0100

    feat: init commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "==> Symlinking zshrc"
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+
+echo "==> Cloning plugin repos"
+[ -d ~/.zsh/zsh-syntax-highlighting ] || \
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+[ -d ~/.zsh/pure ] || \
+  git clone https://github.com/sindresorhus/pure.git ~/.zsh/pure
+
+echo "==> Done! Don't forget to create ~/.zshrc.local if needed."
